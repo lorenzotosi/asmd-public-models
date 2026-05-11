1fbb6f927dd00dfd09b6dab2bae410453a2e7ce0
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2026-05-11 15:03:52 +0200

    refactor QMatrix to imporve extendibility

--- src/main/scala/u09/model/QMatrix.scala
+++ src/main/scala/u09/model/QMatrix.scala
@@ -10,7 +10,7 @@ object QMatrix:
 
   import Move.*
 
-  case class Facade(
+  sealed abstract class AbstractFacade(
                      width: Int,
                      height: Int,
                      initial: Node,
@@ -24,19 +24,23 @@ object QMatrix:
     type State = Node
     type Action = Move
 
+    protected def calculateN2(s: (Int, Int), a: Move): (Int, Int) = (s, a) match
+        case ((n1, n2), UP) => (n1, (n2 - 1) max 0)
+        case ((n1, n2), DOWN) => (n1, (n2 + 1) min (height - 1))
+        case ((n1, n2), LEFT) => ((n1 - 1) max 0, n2)
+        case ((n1, n2), RIGHT) => ((n1 + 1) min (width - 1), n2)
+        case _ => ???
+
     def qEnvironment(): Environment = (s: Node, a: Move) =>
-        // applies direction, without escaping borders
-        val n2: Node = (s, a) match
-          case ((n1, n2), UP) => (n1, (n2 - 1) max 0)
-          case ((n1, n2), DOWN) => (n1, (n2 + 1) min (height - 1))
-          case ((n1, n2), LEFT) => ((n1 - 1) max 0, n2)
-          case ((n1, n2), RIGHT) => ((n1 + 1) min (width - 1), n2)
-          case _ => ???
-        // computes rewards, and possibly a jump
-        (reward.apply((s, a)), jumps.orElse[(Node, Move), Node](_ => n2)(s, a))
+      // applies direction, without escaping borders
+      val n2: Node = calculateN2(s, a)
+      // computes rewards, and possibly a jump
+      (reward.apply((s, a)), jumps.orElse[(Node, Move), Node](_ => n2)(s, a))
 
     def qFunction = QFunction(Move.values.toSet, v0, terminal)
+
     def qSystem = QSystem(environment = qEnvironment(), initial, terminal)
+
     def makeLearningInstance() = QLearning(qSystem, gamma, alpha, epsilon, qFunction)
 
     def show[E](v: Node => E, formatString: String): String =
@@ -45,7 +49,20 @@ object QMatrix:
         col <- 0 until height
       yield formatString.format(v((col, row))) + (if (col == height - 1) "\n" else "\t"))
         .mkString("")
-        
+
+  case class Facade(
+                     width: Int,
+                     height: Int,
+                     initial: Node,
+                     terminal: PartialFunction[Node, Boolean],
+                     reward: PartialFunction[(Node, Move), Double],
+                     jumps: PartialFunction[(Node, Move), Node],
+                     gamma: Double,
+                     alpha: Double,
+                     epsilon: Double = 0.0,
+                     v0: Double)
+    extends AbstractFacade(width, height, initial, terminal, reward, jumps, gamma, alpha, epsilon, v0)
+
   case class FacadeWithFixedObstacles(width: Int,
                                       height: Int,
                                       initial: Node,
@@ -56,32 +73,12 @@ object QMatrix:
                                       gamma: Double,
                                       alpha: Double,
                                       epsilon: Double = 0.0,
-                                      v0: Double) extends QRLImpl:
-    type State = Node
-    type Action = Move
+                                      v0: Double)
+    extends AbstractFacade(width, height, initial, terminal, reward, jumps, gamma, alpha, epsilon, v0):
 
-    def qEnvironment(): Environment = (s: Node, a: Move) =>
-      // applies direction, without escaping borders
-      val n2: Node = (s, a) match
-        case ((n1, n2), UP) => (n1, (n2 - 1) max 0)
-        case ((n1, n2), DOWN) => (n1, (n2 + 1) min (height - 1))
-        case ((n1, n2), LEFT) => ((n1 - 1) max 0, n2)
-        case ((n1, n2), RIGHT) => ((n1 + 1) min (width - 1), n2)
-        case _ => ???
-
+    override def qEnvironment(): Environment = (s: Node, a: Move) =>
+      val n2: Node = calculateN2(s, a)
       if (obstacles.contains(n2))
-        (-100.0, s) // Collision with fixed obstacle: penalty and stay in place
+        (-100.0, s)
       else
-        // computes rewards, and possibly a jump
         (reward.applyOrElse((s, a), _ => 0.0), jumps.orElse[(Node, Move), Node](_ => n2)(s, a))
-
-    def qFunction = QFunction(Move.values.toSet, v0, terminal)
-    def qSystem = QSystem(environment = qEnvironment(), initial, terminal)
-    def makeLearningInstance() = QLearning(qSystem, gamma, alpha, epsilon, qFunction)
-
-    def show[E](v: Node => E, formatString: String): String =
-      (for
-        row <- 0 until width
-        col <- 0 until height
-      yield formatString.format(v((col, row))) + (if (col == height - 1) "\n" else "\t"))
-        .mkString("")
